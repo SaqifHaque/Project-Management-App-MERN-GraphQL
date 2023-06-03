c872c26bb4432d3590dd11b1698b6ca0e9611e2f
Saqif <saqifhaque@gmail.com>
2023-06-03 22:55:38 +0600

    Working on Project schema

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -15,6 +15,19 @@ const ClientType = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
+        projects: {
+            type: new GraphQLList(ProjectType),
+            resolve(parent, args) {
+                return projects;
+            }
+        },
+        project: {
+            type: ProjectType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args) {
+                return projects.find(project => project.id === args.id);
+            }
+        },
         clients: {
             type: new GraphQLList(ClientType),
             resolve(parent, args) {
@@ -30,6 +43,16 @@ const RootQuery = new GraphQLObjectType({
         }
     }
 });
+
+const ProjectType = new GraphQLObjectType({
+    name: 'Project',
+    fields: () => ({
+        id: { type: GraphQLID },
+        name: { type: GraphQLString },
+        description: { type: GraphQLString },
+        status: { type: GraphQLString },
+    })
+})
 
 module.exports = new GraphQLSchema({
     query: RootQuery
