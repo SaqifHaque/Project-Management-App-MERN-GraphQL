3799ae2d28146ed43e2cb0bb3d2056fff0404f21
Saqif <saqifhaque@gmail.com>
2023-06-18 22:56:34 +0600

    cors configuration

--- server/index.js
+++ server/index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 require('dotenv').config();
+const cors = require('cors');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
 const connectDB = require('./config/db');
+
 const port = process.env.port || 5000;
 
 const app = express();
 
 connectDB();
+
+app.use(cors());
 
 app.use('/graphql', graphqlHTTP({
     schema,
