a25d97fc19b767cca257616dc20a56aed6297840
Saqif <saqifhaque@gmail.com>
2023-07-02 20:12:34 +0600

    addClient Modal Mutation configured

--- client/src/components/AddClientModal.jsx
+++ client/src/components/AddClientModal.jsx
@@ -1,13 +1,42 @@
 import { useState } from 'react';
 import { FaUser } from 'react-icons/fa';
+import { ADD_CLIENT } from '../mutations/ClientMutations';
+import { useMutation } from '@apollo/client';
+import { GET_CLIENTS } from '../queries/clientQueries';
 
 const AddClientModal = () => {
   const [ name, setName ] = useState("");
   const [ email, setEmail ] = useState("");
   const [ phone, setPhone ] = useState("");
 
+  const [addClient] = useMutation(ADD_CLIENT, {
+    variables: { name, email, phone },
+    update(cache, { data: { addClient } }) {
+      const { clients } = cache.readQuery({
+        query: GET_CLIENTS
+      });
+
+      cache.writeQuery({
+        query: GET_CLIENTS,
+        data: {
+          clients: clients.concat([...clients, addClient])
+        },
+      });
+    }
+  });
+
   const onSubmit = (e) => {
     e.preventDefault();
+
+    if(name === '' || email === '' || phone === '') {
+      return alert('Please fill in all the fields!')
+    }
+
+    addClient(name, email, phone);
+
+    setName('');
+    setEmail('');
+    setPhone('');
   }
 
   return (
