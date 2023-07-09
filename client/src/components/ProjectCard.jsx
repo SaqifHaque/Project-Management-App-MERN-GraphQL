04213817674b34e1c45b7a865c6a3b0b353059d9
Saqif <saqifhaque@gmail.com>
2023-07-09 12:31:51 +0600

    Working on ProjectCard Component

--- client/src/components/ProjectCard.jsx
+++ client/src/components/ProjectCard.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
-const ProjectCard = () => {
+const ProjectCard = ({ project }) => {
   return (
-    <div>ProjectCard</div>
+    <div className="col-md-4">
+        <div className="card mb-3">
+            <div className="d-flex justify-content-between align-items-center">
+                <div className="card-title">{project.name}</div>
+            </div>
+        </div>
+    </div>
   )
 }
 
