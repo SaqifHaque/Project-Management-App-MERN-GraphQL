7eeef8c4b7a05848fe5d64ed9746045025bb9e50
SaqifHaque <saqifhaque@gmail.com>
2023-10-05 21:39:48 +0600

    Working on Project Component to show project details

--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
+import { Link, useParams } from 'react-router-dom';
+import Spinner from '../components/Spinner';
+import { GET_PROJECT } from '../queries/projectQueries';
 
 const Project = () => {
+  const { id } = useParams();
+  const { loading, error, data } = useQuery(GET_PROJECT, { variables: { id } });
+
+  if(loading) return <Spinner />
+  if(error) return <p>Something went Wrong</p>
   return (
     <div>Project</div>
   )
